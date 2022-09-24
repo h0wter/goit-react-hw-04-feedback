da92a9d206ade48302bf5fa6d5526fd95d9870bf
h0wter <howter99@gmail.com>
2022-09-24 18:09:09 +0300

    добавляет компонент feedback и его стили

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+import { Button } from './FeedbackOptions.styled';
+import { Box } from 'components/Box';
+
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <>
+      <Box as="ul" display="flex" mb="20px">
+        {options.map(option => {
+          return (
+            <Box as="li" mr="10px" key={option}>
+              <Button
+                name={option}
+                onClick={e => {
+                  onLeaveFeedback(e);
+                }}
+              >
+                {option}
+              </Button>
+            </Box>
+          );
+        })}
+      </Box>
+    </>
+  );
+};
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.array.isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
---
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  text-transform: capitalize;
+  font-weight: 500;
+  padding: 2px 6px;
+  background-color: transparent;
+  border: 1px solid;
+  border-radius: 5px;
+  cursor: pointer;
+`;
